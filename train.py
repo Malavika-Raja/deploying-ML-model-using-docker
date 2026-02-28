86b9b4490bd96c59f3bdbbbdf0ab399927e425d2
Malavika Raja <malurv30@gmail.com>
2026-02-28 13:45:53 +0530

    Added train.py file

---
+++ train.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import fetch_california_housing
+import joblib
+
+housing = fetch_california_housing()
+X = pd.DataFrame(housing.data, columns=housing.feature_names)
+y =pd.Series(housing.target, name="MedHouseVal")
+
+features_to_use = ['MedInc','HouseAge','AveRooms','AveBedrms','Population']
+
+X=X[features_to_use]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=64)
+
+model = RandomForestRegressor(n_estimators=100,random_state=42)
+
+model.fit(X_train, y_train)
+
+model_filename ='california_housing_model.joblib'
+joblib.dump(model,model_filename)
+
+print(f"Model saved as {model_filename}. Training script is done!")
