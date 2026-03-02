edc0684bfc82664debf18ad59bb3ee69759f2fa5
Malavika Raja <malurv30@gmail.com>
2026-03-02 15:42:48 +0530

    Modified train.py to train the model when the app starts

--- dock/train.py
+++ dock/train.py
@@ -4,17 +4,21 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import fetch_california_housing
 import joblib
 
-housing = fetch_california_housing()
-X = pd.DataFrame(housing.data, columns=housing.feature_names)
-y =pd.Series(housing.target, name="MedHouseVal")
+def train_and_save_model():
+    housing = fetch_california_housing()
+    X = pd.DataFrame(housing.data, columns=housing.feature_names)
+    y =pd.Series(housing.target, name="MedHouseVal")
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=64)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=64)
+    model = RandomForestRegressor(n_estimators=100,random_state=42)
 
-model = RandomForestRegressor(n_estimators=100,random_state=42)
+    model.fit(X_train, y_train)
 
-model.fit(X_train, y_train)
+    model_filename ='california_housing_model.joblib'
+    joblib.dump(model,model_filename)
 
-model_filename ='california_housing_model.joblib'
-joblib.dump(model,model_filename)
+    print(f"Model saved as {model_filename}. Training script is done!")
 
-print(f"Model saved as {model_filename}. Training script is done!")
+if __name__ == "__main__":
+    train_and_save_model()
