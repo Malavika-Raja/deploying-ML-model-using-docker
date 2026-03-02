6cc24f9ec8e877caebd315261ebdc4576d75b1d7
Malavika Raja <malurv30@gmail.com>
2026-03-02 12:45:16 +0530

    Modified .gitignore file

---
+++ dock/main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, ConfigDict
+import joblib
+import numpy as np
+# main application object
+app = FastAPI(title ="California Housing Price Prediction API")
+# loaded the pre-trained model into memory
+model = joblib.load("california_housing_model.joblib")
+
+#defines exact shape and data types of the input
+class HouseFeatures(BaseModel):
+    MedInc: float
+    HouseAge: float
+    AveRooms: float
+    AveBedrms: float
+    Population: float
+    AveOccup: float
+    Latitude: float
+    Longitude: float
+
+    model_config = ConfigDict(
+        json_schema_extra = {
+            "example": {
+               "MedInc" : 8.3253,
+               "HouseAge" : 41.0,
+               "AveRooms" : 6.9841,
+               "AveBedrms" : 1.0238,
+               "Population" : 322.0,
+               "AveOccup": 3.21,
+               "Latitude":37.77,
+               "Longitude":-122.42
+            }
+        }
+    )
+
+#prediction endpoint, listens to PO             ST requests at that URL
+@app.post("/predict", tags=["Predictions"])
+async def predict_price(features: HouseFeatures):
+    """
+    Predicts the median house value(target variable) based on input features
+    """
+    # Convert the input data into a Numpy array
+    # the order of features must be the same as the training
+    input_data = np.array([[
+        features.MedInc,
+        features.HouseAge,
+        features.AveRooms,
+        features.AveBedrms,
+        features.Population,
+        features.AveOccup,
+        features.Latitude,
+        features.Longitude
+]])
+    # the output is a price in units of 100,000 USD
+    prediction = model.predict(input_data)[0]
+
+    return {"predicted_median_house_value":f"${prediction * 100000:.2f}"}
+
+@app.get("/",tags=["General"])
+async def read_root():
+    return {"message":"Welcome to the Housing Price Prediction API!"}
--- dock/train.py
+++ dock/train.py
@@ -8,10 +8,6 @@ housing = fetch_california_housing()
 X = pd.DataFrame(housing.data, columns=housing.feature_names)
 y =pd.Series(housing.target, name="MedHouseVal")
 
-features_to_use = ['MedInc','HouseAge','AveRooms','AveBedrms','Population']
-
-X=X[features_to_use]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=64)
 
 model = RandomForestRegressor(n_estimators=100,random_state=42)
